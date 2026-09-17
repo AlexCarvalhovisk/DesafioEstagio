4fa3b06b3bbd30ecb9d80b883efb4f53443f8ad8
Alex <adscarvalho@restinga.ifrs.edu.br>
2026-09-17 10:46:58 -0300

    Primeiro commit do desafio. Entidade já criada.

---
+++ DesafioEstagio/src/main/java/br/com/alexcarvalho/DesafioEstagio/entity/Tarefa.java
@@ -0,0 +1,60 @@
+package br.com.alexcarvalho.DesafioEstagio.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String titulo;
+    public String descricao;
+    public LocalDate dataEntrega;
+    public boolean concluida;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public void setDataEntrega(LocalDate dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+}
---
+++ DesafioEstagio/src/test/java/br/com/alexcarvalho/DesafioEstagio/DesafioEstagioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alexcarvalho.DesafioEstagio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEstagioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
